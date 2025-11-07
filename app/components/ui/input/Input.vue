7b6236223b80f7f98cd3c247c1a81ff75a57318c
Hugh Tian <sanagawa441@gmail.com>
2025-11-07 14:42:26 -0500

    Input.vue
    Function type instead of object literal.

--- app/components/ui/input/Input.vue
+++ app/components/ui/input/Input.vue
@@ -3,15 +3,15 @@ import type { HTMLAttributes } from "vue"
 import { useVModel } from "@vueuse/core"
 import { cn } from "@/lib/utils"
 
-const props = defineProps<{
+const props = defineProps<
   defaultValue?: string | number
   modelValue?: string | number
   class?: HTMLAttributes["class"]
-}>()
+>()
 
-const emits = defineEmits<{
+const emits = defineEmits<
   (e: "update:modelValue", payload: string | number): void
-}>()
+>()
 
 const modelValue = useVModel(props, "modelValue", emits, {
   passive: true,
